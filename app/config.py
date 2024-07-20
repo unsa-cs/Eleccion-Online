7a092ae74ccf01184815de04f2f6b32227c94abc
chuanca26 <chuancao@unsa.edu.pe>
2024-07-20 11:06:45 -0500

    agregando models listaCandidato

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@
 
 class Config:
     # Configuración de la base de datos MySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:admin@localhost/eleccion'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:chuancao26@localhost/eleccion'
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactivar el seguimiento de modificaciones
