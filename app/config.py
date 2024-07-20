8bf0394123ab9cb1e435832f619449a479c5160b
chuanca26 <chuancao@unsa.edu.pe>
2024-07-20 11:20:35 -0500

    cambios

--- app/config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-
-class Config:
-    # Configuración de la base de datos MySQL
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:chuancao26@localhost/eleccion'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Desactivar el seguimiento de modificaciones
