d6d8b34e5efcddb40664c81cfd5fb1570ba81c8c
sebas123-code <smendozaf@unsa.edu.pe>
2024-07-24 15:20:38 -0500

    Se configuro para que use variable globales

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 import logging
 from app.config import Config
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 db = SQLAlchemy()
 def create_app():
     app = Flask(__name__, static_folder='static', static_url_path='/static')
     app.config.from_object(Config)
 
     logging.basicConfig(level=logging.INFO)
-    
+
     db.init_app(app)
 
     from app.routes.routes import home_bp
